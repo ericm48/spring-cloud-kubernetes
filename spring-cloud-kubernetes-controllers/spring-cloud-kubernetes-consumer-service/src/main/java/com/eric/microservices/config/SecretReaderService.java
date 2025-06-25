39bd5dfeb3514d084e1a7f6520bdcea2c494fc4c
ericm48 <me_manley@yahoo.com>
2025-06-25 13:22:41 -0600

    -Updated SecretReader for evars, bumped version.

--- spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-consumer-service/src/main/java/com/eric/microservices/config/SecretReaderService.java
+++ spring-cloud-kubernetes-controllers/spring-cloud-kubernetes-consumer-service/src/main/java/com/eric/microservices/config/SecretReaderService.java
@@ -4,24 +4,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class SecretReaderService {
 
+    @Value("${SCK_USERID}")
+    private String filePathUserID;
+
+    @Value("${SCK_PASSWORD}")
+    private String filePathPassword;
+
     public String getUserId1FromFile() throws IOException {
 
-        // Assuming the secret file is named 'TEST_ERIC_TEST_USERID1' and mounted at /dsg-platform/secrets
+        String returnValue = null;
 
-        String filePath = "/dsg-platform/secrets/TEST_ERIC_TEST_USERID1";
-        return new String(Files.readAllBytes(Paths.get(filePath)));
+        if (filePathUserID != null) {
+
+            returnValue = new String(Files.readAllBytes(Paths.get(filePathUserID)));
+            log.debug("SecretReaderService.getUserId1FromFile() returnValue: " + returnValue);
+        }
+
+        return returnValue;
     }
 
     public String getPassword1FromFile() throws IOException {
 
-        // Assuming the secret file is named 'TEST_ERIC_TEST_PASSWORD1' and mounted at /dsg-platform/secrets
+        String returnValue = null;
+        
+        if (filePathUserID != null) {
 
-        String filePath = "/dsg-platform/secrets/TEST_ERIC_TEST_PASSWORD1";
-        return new String(Files.readAllBytes(Paths.get(filePath)));
+            returnValue = new String(Files.readAllBytes(Paths.get(filePathPassword)));
+            log.debug("SecretReaderService.getPassword1FromFile() returnValue: " + returnValue);            
+        }
+
+        return returnValue;
     }        
 }
